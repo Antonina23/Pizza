8ef96c7cf1df31823833a566b66483653ba35a88
Antonina23 <tone4kawow@gmail.com>
2018-11-13 23:31:12 +0300

    Add some type of pizza

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_183850) do
+ActiveRecord::Schema.define(version: 2018_11_13_191410) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
