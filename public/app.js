56f109a40bfa88ea9da985bb0bb21fef650d7ea4
Antonina23 <tone4kawow@gmail.com>
2018-11-15 21:42:40 +0300

    Add function add_to_cart

--- public/app.js
+++ public/app.js
@@ -1,10 +1,15 @@
-
 function something()
-	{
+	 {
 		var x = window.localStorage.getItem('bbb');
-		// в руби это x = hh['bbb']
-		x = x * 1 + 1; // x = x + 1
+		 //в руби это x = hh['bbb']
+		 x = x * 1 + 1; // x = x + 1
 
 		window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 		alert(x);
-	}
+	 }
+
+function add_to_cart()
+{
+	alert('Hello from function');
+}
+
